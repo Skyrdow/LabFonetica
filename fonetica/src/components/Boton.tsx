8be908a8e1b176116ebb458ee80d8ad68a992587
Klefur <joaquinalberto55@gmail.com>
2023-04-19 11:57:37 -0400

    chore: se crea componente Header

--- fonetica/src/components/Boton.tsx
+++ fonetica/src/components/Boton.tsx
@@ -3,7 +3,6 @@ interface Props {
     label: string;
   }
 
-
 export default function Boton({label, href}: Props) {
     return (
         <button className="rounded-lg bg-usach-daisy-900">
---
+++ fonetica/src/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header className="bg-usach-industrial-100 p-3 flex">
+            <img src="/Usach S1.png" alt="Logo Usach" className="h-40 pb-5" />
+            <h1 className="mx-80 my-10 text-6xl font-mono" >Laboratorio de fonética</h1>
+        </header>
+    )
+}
