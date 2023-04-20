7d81b30caa466d13491a33fef337f819550461b7
Klefur <joaquinalberto55@gmail.com>
2023-04-20 14:58:14 -0400

    chore: se asigna mas espacio a contenido y menos a footer

--- fonetica/src/components/Footer.tsx
+++ fonetica/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="bg-usach-aqua-700 p-3 flex-auto">
+        <footer className="bg-usach-aqua-700 p-3">
             <h1 className="mx-80 my-10 font-mono" >Laboratorio de fonética</h1>
         </footer>
     )
