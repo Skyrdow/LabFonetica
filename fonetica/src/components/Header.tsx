7d93b93048893e39d0deafa16140f447d9d07648
Don-Uldaricio <nicolas.aguilera.g@usach.cl>
2023-04-20 02:19:33 -0400

    chore: ajuste title

--- fonetica/src/components/Header.tsx
+++ fonetica/src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
-        <header className="bg-usach-industrial-100 p-3 flex">
+        <header className="bg-usach-industrial-100 p-2 flex">
             <img src="/Usach S1.png" alt="Logo Usach" className="h-40 pb-5" />
-            <h1 className="mx-80 my-10 text-6xl font-mono" >Laboratorio de fonética</h1>
+            <h1 className="mx-20 my-10 text-3xl font-mono">Laboratorio de fonética</h1>
         </header>
     )
 }
