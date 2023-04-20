049bbb0d5f1b42fddf2771af9ec8912b6acb5abb
Don-Uldaricio <nicolas.aguilera.g@usach.cl>
2023-04-20 13:23:09 -0400

    chore: botón logo usach

--- fonetica/src/components/Header.tsx
+++ fonetica/src/components/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
         <header className="bg-usach-industrial-100 p-2 flex">
-            <img src="/Usach S1.png" alt="Logo Usach" className="h-40" />
+            <button>
+                <a href="/home">
+                    <img src="/Usach S1.png" alt="Logo Usach" className="h-40" />
+                </a>
+            </button>
             <h1 className="m-auto text-3xl font-mono align-middle">Laboratorio de fonética</h1>
         </header>
     )
