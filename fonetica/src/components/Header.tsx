e82b05ed6e4385a108b36a95e6eaab217cc6644b
Don-Uldaricio <nicolas.aguilera.g@usach.cl>
2023-04-20 13:17:35 -0400

    chore: se agrega login y ajuste de header

--- fonetica/src/components/Header.tsx
+++ fonetica/src/components/Header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
     return (
         <header className="bg-usach-industrial-100 p-2 flex">
-            <img src="/Usach S1.png" alt="Logo Usach" className="h-40 pb-5" />
-            <h1 className="mx-20 my-10 text-3xl font-mono">Laboratorio de fonética</h1>
+            <img src="/Usach S1.png" alt="Logo Usach" className="h-40" />
+            <h1 className="m-auto text-3xl font-mono align-middle">Laboratorio de fonética</h1>
         </header>
     )
 }
