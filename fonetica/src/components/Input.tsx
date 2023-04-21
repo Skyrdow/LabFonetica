5cae94de95b1f02d677e0a682dac7ce99d01073d
Skyrdow <lucas.mesias@usach.cl>
2023-04-21 10:57:46 -0400

    refactor: texto de input alineado a la izquierda

--- fonetica/src/components/Input.tsx
+++ fonetica/src/components/Input.tsx
@@ -9,14 +9,14 @@ export default function Input({Label, forLabel, type, placeholder }: Props) {
     return(
         <div className="flex flex-col mb-6 items-center">
             <label htmlFor={forLabel} 
-                className="mb-2 text-sm font-medium text-gray-900">
+                className="w-3/4 px-1 text-left mb-2 text-sm font-medium text-gray-900">
                 {Label}
             </label>
             <input type={type} id={forLabel} 
                 className="flex bg-white border border-gray-300 
                     text-gray-900 text-sm rounded-lg 
                     focus:ring-blue-500 focus:border-blue-500 
-                    w-3/4 p-2.5 text-center"
+                    w-3/4 p-2.5 text-left"
                     placeholder={placeholder} required />
         </div>
     )
