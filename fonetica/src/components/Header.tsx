754579546f7c24475f9631044a56a5a41968905f
Skyrdow <lucas.mesias@usach.cl>
2023-04-21 10:33:54 -0400

    chore: cambios en el header
    Nuevo título de la página e hipervínculo del logo a usach.cl

--- fonetica/src/components/Header.tsx
+++ fonetica/src/components/Header.tsx
@@ -2,11 +2,11 @@ export default function Header() {
     return (
         <header className="bg-usach-industrial-100 p-2 flex">
             <button>
-                <a href="/">
+                <a href="https://www.usach.cl/">
                     <img src="/Usach S1.png" alt="Logo Usach" className="h-40" />
                 </a>
             </button>
-            <h1 className="m-auto text-3xl font-mono align-middle">Laboratorio de fonética</h1>
+            <h1 className="m-auto text-3xl font-mono align-middle">Recursos Virtuales para el aprendizaje del Lenugaje</h1>
         </header>
     )
 }
