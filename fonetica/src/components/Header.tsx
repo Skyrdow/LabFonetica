f62c56539ed38e533d5dcba31bbeb71f339b8597
Skyrdow <lucas.mesias@usach.cl>
2023-04-21 21:10:59 -0400

    refactor: cambios en el header y en index
    el title de index estaba desactualizado

--- fonetica/src/components/Header.tsx
+++ fonetica/src/components/Header.tsx
@@ -1,14 +1,12 @@
 export default function Header() {
     return (
-        <header className="bg-usach-industrial-1000 p-2 flex">
-            <div className="h-40 flex flex-row gap-10">
-                <button>
-                    <a href="/" className="h-max">
-                        <img src="/Usach SB.png" alt="Logo Usach" className="h-max"/>
-                    </a>
-                </button>
-                <h1 className="m-auto text-3xl font-mono text-white align-middle">Recursos Virtuales para el aprendizaje del Lenguaje</h1>
-            </div>
+        <header className="bg-usach-industrial-1000 p-2 gap-10 flex">
+            <button className="w-1/3">
+                <a href="/" className="">
+                    <img src="/Usach SB.png" alt="Logo Usach" className=""/>
+                </a>
+            </button>
+            <h1 className="m-auto text-3xl font-mono text-white align-middle">Recursos Virtuales para el aprendizaje del Lenguaje</h1>
         </header>
     )
 }
