cd7f7e2ea0376044363a381ad1046710db9cd2a5
Skyrdow <lucas.mesias@usach.cl>
2023-04-24 22:33:32 -0400

    refactor: cambios para el estilo nuevo

--- fonetica/src/components/BotonJuego.tsx
+++ fonetica/src/components/BotonJuego.tsx
@@ -6,7 +6,7 @@ interface Props {
   
 export default function BotonJuego({ label, href, type }: Props) {
     return (
-        <button className="rounded-lg bg-usach-terra-800 hover:bg-usach-ultra-800 text-5xl text-white my-3 drop-shadow p-4 px-8" itemType={type}>
+        <button className="rounded-lg bg-usach-daisy-800 hover:bg-usach-ultra-800 text-5xl text-white my-3 drop-shadow p-4 px-8" itemType={type}>
             <a href={href}>{label}</a>
         </button>
     );
